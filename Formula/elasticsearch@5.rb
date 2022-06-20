5c457c53df1e7776b77f393b3dc5c867157b37f3
Chris Kruger <montdidier@users.noreply.github.com>
2022-06-20 16:55:48 +0800

    no need bottle arg

--- Formula/elasticsearch@5.rb
+++ Formula/elasticsearch@5.rb
@@ -5,7 +5,6 @@ class ElasticsearchAT5 < Formula
   sha256 "6b035a59337d571ab70cea72cc55225c027ad142fbb07fd8984e54261657c77f"
   license "Apache-2.0"
 
-  bottle :unneeded
   keg_only :versioned_formula
 
   depends_on "openjdk@8"
